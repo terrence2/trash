f43f9826cf00c4be4adeaef23cded7cb6fcd6c92
Terrence Cole <terrence@trainedmonkeystudios.org>
2010-09-21 19:42:41 -0700

    license Trash under the GPLv3

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,20 @@
 {-
  - Copyright (c) 2010, Terrence Cole.
+ -
+ - This file is part of Trash, Terrence's Re-Bourne Again SHell.
+ -
+ - Trash is free software: you can redistribute it and/or modify
+ - it under the terms of the GNU General Public License as published by
+ - the Free Software Foundation, either version 3 of the License, or
+ - (at your option) any later version.
+ -
+ - Trash is distributed in the hope that it will be useful,
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ - GNU General Public License for more details.
+ -
+ - You should have received a copy of the GNU General Public License
+ - along with Trash.  If not, see <http://www.gnu.org/licenses/>.
  -}
 module Main where
 import Interact
